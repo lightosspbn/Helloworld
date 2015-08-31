150de6d1d79b8a9af36f7d02cc23ff3a2bc9dfc8
sallypbn <345311487@qq,com>
2015-08-31 14:02:28 +0800

    version for helloworld 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Hello World1!");
+		 System.out.println("Hello World2!");
+		 
 	}
 
 }

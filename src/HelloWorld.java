d0a666c383868fae51d4787fc72e67f4633c2e36
sallypbn <345311487@qq,com>
2015-08-31 14:17:40 +0800

    for version double 1-3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,9 +6,9 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 System.out.println("Hello World1!");
-		 System.out.println("Hello World2!");
-		 System.out.println("Hello World3!");
+		 System.out.println("Hello World11!");
+		 System.out.println("Hello World22!");
+		 System.out.println("Hello World33!");
 		 
 	}
 
